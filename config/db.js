be472359279f6a30aeb21768ab5364c9728cc510
janniks <jannik.sieb@gmail.com>
2016-02-18 19:35:08 +0100

    add db config

---
+++ config/db.js
@@ -0,0 +1,46 @@
+/**
+ * db.js
+ * 
+ * Configure the database for boyle
+ * Default values are commented out below
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+
+// Configure Globals
+module.exports = {
+
+	/*
+	 * address: The address of your database
+	 * default: 'localhost'
+	 * accepted: string
+	 */
+	// 'address': 'localhost',
+
+	/*
+	 * port: The port of your database
+	 * default: ''
+	 * accepted: string
+	 */
+	// 'port': '',
+
+	/*
+	 * username: The username of your database
+	 * default: ''
+	 * accepted: string
+	 */
+	// 'username': '',
+	
+	/*
+	 * password: The password of your database
+	 * default: ''
+	 * accepted: string
+	 */
+	// 'password': '',
+	
+	/*
+	 * database: The database name of your database
+	 * default: ''
+	 * accepted: string
+	 */
+	// 'database': '',
+	
+};
