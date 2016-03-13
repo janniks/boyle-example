27f2b6ef04414849920de784760d8a99ad7b016d
janniks <jannik.sieb@gmail.com>
2016-03-13 01:17:02 +0100

    add web config file

---
+++ config/web.js
@@ -0,0 +1,18 @@
+/**
+ * web.js
+ * 
+ * Configure the settings for the web of the boilerplate
+ * Default values are commented out below
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+
+// Configure Globals
+module.exports = {
+	
+	/*
+	 * static: Determines which directories are accessible statically.
+	 * default: 'public'
+	 * accepted: string, [string]
+	 */
+	// 'static': 'public',
+
+};
